1f74ea89cd33e8caf818916cb34f00b219b80e4a
Sam <swizrc@mst.edu>
2017-11-03 15:16:00 -0500

    Add project files.

---
+++ TryThis/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace TechJobsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two Dictionary vars to hold info for menu and data
+            
+            // Top-level menu options
+            Dictionary<string, string> actionChoices = new Dictionary<string, string>();
+            actionChoices.Add("search", "Search");
+            actionChoices.Add("list", "List");
+
+            // Column options
+            Dictionary<string, string> columnChoices = new Dictionary<string, string>();
+            columnChoices.Add("core competency", "Skill");
+            columnChoices.Add("employer", "Employer");
+            columnChoices.Add("location", "Location");
+            columnChoices.Add("position type", "Position Type");
+            columnChoices.Add("all", "All");
+
+            Console.WriteLine("Welcome to LaunchCode's TechJobs App!");
+            // Allow user to search/list until they manually quit with ctrl+c
+            while (true)
+            {
+
+                string actionChoice = GetUserSelection("View Jobs", actionChoices);
+
+                if (actionChoice.Equals("list"))
+                {
+                    string columnChoice = GetUserSelection("List", columnChoices);
+
+                    if (columnChoice.Equals("all"))
+                    {
+                        PrintJobs(JobData.FindAll());
+                    }
+                    else
+                    {
+                        List<string> results = JobData.FindAll(columnChoice);
+
+                        Console.WriteLine("\n*** All " + columnChoices[columnChoice] + " Values ***");
+                        foreach (string item in results)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                }
+                else // choice is "search"
+                {
+                    // How does the user want to search (e.g. by skill or employer)
+                    string columnChoice = GetUserSelection("Search", columnChoices);
+
+                    // What is their search term?
+                    Console.WriteLine("\nSearch term: ");
+                    string searchTerm = Console.ReadLine();
+
+                    List<Dictionary<string, string>> searchResults;
+
+                    // Fetch results
+                    if (columnChoice.Equals("all"))
+                    {
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
+                    }
+                    else
+                    {
+                        searchResults = JobData.FindByColumnAndValue(columnChoice, searchTerm);
+                        PrintJobs(searchResults);
+                    }
+                }
+            }
+        }
+
+        /*
+         * Returns the key of the selected item from the choices Dictionary
+         */
+        private static string GetUserSelection(string choiceHeader, Dictionary<string, string> choices)
+        {
+            int choiceIdx;
+            bool isValidChoice = false;
+            string[] choiceKeys = new string[choices.Count];
+
+            int i = 0;
+            foreach (KeyValuePair<string, string> choice in choices)
+            {
+                choiceKeys[i] = choice.Key;
+                i++;
+            }
+
+            do
+            {
+                Console.WriteLine("\n" + choiceHeader + " by:");
+
+                for (int j = 0; j < choiceKeys.Length; j++)
+                {
+                    Console.WriteLine(j + " - " + choices[choiceKeys[j]]);
+                }
+
+                string input = Console.ReadLine();
+                choiceIdx = int.Parse(input);
+
+                if (choiceIdx < 0 || choiceIdx >= choiceKeys.Length)
+                {
+                    Console.WriteLine("Invalid choices. Try again.");
+                }
+                else
+                {
+                    isValidChoice = true;
+                }
+
+            } while (!isValidChoice);
+
+            return choiceKeys[choiceIdx];
+        }
+
+        public static StringBuilder Elongate(StringBuilder myString)
+        {
+            if (myString.Length == 15)
+            {
+                return myString;
+            }
+            else
+            {
+                return Elongate(myString.Append(" "));
+            }
+        } 
+
+        private static void PrintJobs(List<Dictionary<string, string>> someJobs)
+        {
+            if(someJobs.Count != 0)
+            {
+                foreach (Dictionary<string, string> job in someJobs)
+                {
+                    Console.WriteLine("****************");
+                    foreach (string key in job.Keys)
+                    {
+                        //maxlen = 15
+                        StringBuilder keyBuilder = new StringBuilder();
+                        keyBuilder.Append(key);
+                        keyBuilder = Elongate(keyBuilder);
+                        Console.WriteLine(keyBuilder + " : " + job[key]);
+                    }
+                    Console.WriteLine("\n");
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("No search results to display!");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
